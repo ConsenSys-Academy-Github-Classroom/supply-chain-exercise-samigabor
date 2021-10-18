d113c320188c455591a89fcd26a0cbf82e07489f
s-gabor <ing.gabor@yahoo.com>
2021-10-18 14:55:28 +0300

    Define state variables, events, modifiers

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -2,69 +2,6 @@
 pragma solidity >=0.5.16 <0.9.0;
 
 contract SupplyChain {
-
-  // <owner>
-
-  // <skuCount>
-
-  // <items mapping>
-
-  // <enum State: ForSale, Sold, Shipped, Received>
-
-  // <struct Item: name, sku, price, state, seller, and buyer>
-  
-  /* 
-   * Events
-   */
-
-  // <LogForSale event: sku arg>
-
-  // <LogSold event: sku arg>
-
-  // <LogShipped event: sku arg>
-
-  // <LogReceived event: sku arg>
-
-
-  /* 
-   * Modifiers
-   */
-
-  // Create a modifer, `isOwner` that checks if the msg.sender is the owner of the contract
-
-  // <modifier: isOwner
-
-  modifier verifyCaller (address _address) { 
-    // require (msg.sender == _address); 
-    _;
-  }
-
-  modifier paidEnough(uint _price) { 
-    // require(msg.value >= _price); 
-    _;
-  }
-
-  modifier checkValue(uint _sku) {
-    //refund them after pay for item (why it is before, _ checks for logic before func)
-    _;
-    // uint _price = items[_sku].price;
-    // uint amountToRefund = msg.value - _price;
-    // items[_sku].buyer.transfer(amountToRefund);
-  }
-
-  // For each of the following modifiers, use what you learned about modifiers
-  // to give them functionality. For example, the forSale modifier should
-  // require that the item with the given sku has the state ForSale. Note that
-  // the uninitialized Item.State is 0, which is also the index of the ForSale
-  // value, so checking that Item.State == ForSale is not sufficient to check
-  // that an Item is for sale. Hint: What item properties will be non-zero when
-  // an Item has been added?
-
-  // modifier forSale
-  // modifier sold(uint _sku) 
-  // modifier shipped(uint _sku) 
-  // modifier received(uint _sku) 
-
   constructor() public {
     // 1. Set the owner to the transaction sender
     // 2. Initialize the sku count to 0. Question, is this necessary?
@@ -130,4 +67,84 @@ contract SupplyChain {
   /*   buyer = items[_sku].buyer; */
   /*   return (name, sku, price, state, seller, buyer); */
   /* } */
+    address public owner;
+
+    uint public skuCount;
+
+    mapping(uint => Item) public items;
+
+    enum State {
+        ForSale,
+        Sold,
+        Shipped,
+        Received
+    }
+
+    struct Item {
+        string name;
+        uint sku;
+        uint price;
+        State state;
+        address payable seller;
+        address payable buyer;
+    }
+
+    /*
+     * Events
+     */
+
+    event LogForSale(uint sku);
+    event LogSold(uint sku);
+    event LogShipped(uint sku);
+    event LogReceived(uint sku);
+
+    /*
+     * Modifiers
+     */
+
+    modifier isOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier verifyCaller(address _address) {
+        require(msg.sender == _address);
+        _;
+    }
+
+    modifier paidEnough(uint _price) {
+        require(msg.value >= _price);
+        _;
+    }
+
+    modifier checkValue(uint _sku) {
+        _;
+        uint _price = items[_sku].price;
+        uint amountToRefund = msg.value - _price;
+        items[_sku].buyer.transfer(amountToRefund);
+    }
+
+    modifier forSale(uint sku) {
+        require(
+            items[sku].state == State.ForSale && items[sku].price > 0,
+            "Item not for sale."
+        );
+        _;
+    }
+
+    modifier sold(uint sku) {
+        require(items[sku].state == State.Sold, "Item not sold.");
+        _;
+    }
+
+    modifier shipped(uint sku) {
+        require(items[sku].state == State.Shipped, "Item not shipped.");
+        _;
+    }
+
+    modifier received(uint sku) {
+        require(items[sku].state == State.Received, "Item not received.");
+        _;
+    }
+
 }
